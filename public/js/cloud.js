316bf30f6e43bcd9dda827bdf6016d557de9e0e9
xleonard1 <xavier.lnrd@gmail.com>
2022-06-10 20:45:38 -0400

    cloudinary javascript function to pull in cloudinary image

---
+++ public/js/cloud.js
@@ -0,0 +1,30 @@
+// 1. Import classes
+//===================
+
+// Import the Cloudinary class.
+import {Cloudinary} from "@cloudinary/url-gen";
+import {Transformation} from "@cloudinary/url-gen";
+
+// Import required actions.
+import {thumbnail, scale} from "@cloudinary/url-gen/actions/resize";
+import {byRadius} from "@cloudinary/url-gen/actions/roundCorners";
+import {sepia} from "@cloudinary/url-gen/actions/effect";
+import {source} from "@cloudinary/url-gen/actions/overlay";
+import {opacity,brightness} from "@cloudinary/url-gen/actions/adjust";
+import {byAngle} from "@cloudinary/url-gen/actions/rotate";
+
+// Import required qualifiers.
+import {image} from "@cloudinary/url-gen/qualifiers/source";
+import {Position} from "@cloudinary/url-gen/qualifiers/position";
+import {compass} from "@cloudinary/url-gen/qualifiers/gravity";
+import {focusOn} from "@cloudinary/url-gen/qualifiers/gravity";
+import {FocusOn} from "@cloudinary/url-gen/qualifiers/focusOn";
+
+// Import any actions required for transformations.
+import {fill} from "@cloudinary/url-gen/actions/resize";
+
+
+// 2. Set your cloud name
+//========================
+
+// Create a Cloudinary instance and set your cloud name.

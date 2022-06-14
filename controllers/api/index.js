127747dee788522c14f998a4b364713572a62e7c
xleonard1 <xavier.lnrd@gmail.com>
2022-06-14 12:49:15 -0400

    debugged broken files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./gameRoutes');
+const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
